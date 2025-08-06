600d459a9fcad12e38568a68326d04dbaa180363
DANRAMAGE <ChiefDan@gmail.com>
2025-08-06 16:29:16 -0400

    Added datasaver that can talk postgres or sqlite.

--- xeniadbutilities/database_settings.py
+++ xeniadbutilities/database_settings.py
@@ -2,19 +2,36 @@ from configparser import SafeConfigParser
 
 class DatabaseConfiguration:
     def __init__(self, db_type, **kwargs):
-        self.db_type = db_type.lower()
 
-        if self.db_type == "postgres":
-            if 'ini_file' not in kwargs:
-                self.username = kwargs.get("username")
-                self.password = kwargs.get("password")
-                self.host = kwargs.get("host", "localhost")
-                self.port = kwargs.get("port", 5432)
-                self.database_name = kwargs.get("database_name")
-                self.connectionstring = kwargs.get("connectionstring", None)
+        self.db_type = None
+        self.username = None
+        self.password = None
+        self.host = None
+        self.port = None
+        self.database_name =None
+        self.connectionstring = None
+
+        if 'ini_file' not in kwargs:
+            self.db_type = db_type.lower()
+            if self.db_type == "postgres":
+                if 'ini_file' not in kwargs:
+                    self.username = kwargs.get("username")
+                    self.password = kwargs.get("password")
+                    self.host = kwargs.get("host", "localhost")
+                    self.port = kwargs.get("port", 5432)
+                    self.database_name = kwargs.get("database_name")
+                    self.connectionstring = kwargs.get("connectionstring", None)
+            elif self.db_type == "sqlite":
+                if 'ini_file' not in kwargs:
+                    self.file_path = kwargs.get("file_path")
             else:
-                config_file = SafeConfigParser()
-                config_file.read(kwargs['ini_file'])
+                raise ValueError("Unsupported database type. Use 'postgres' or 'sqlite'.")
+
+        else:
+            config_file = SafeConfigParser()
+            config_file.read(kwargs['ini_file'])
+            db_type = config_file.get('database', 'db_type')
+            if db_type == "postgres":
                 self.username = config_file.get("Database", "user")
                 self.password = config_file.get("Database","password")
                 self.host = config_file.get("Database","host")
@@ -22,16 +39,13 @@ class DatabaseConfiguration:
                 self.database_name = config_file.get("Database", "name")
                 self.connectionstring = config_file.get("Database", "connectionstring")
 
-        elif self.db_type == "sqlite":
-            if 'ini_file' not in kwargs:
-                self.file_path = kwargs.get("file_path")
-            else:
+            elif self.db_type == "sqlite":
                 config_file = SafeConfigParser()
                 config_file.read(kwargs['ini_file'])
                 self.file_path = kwargs.get("Database", "file_path")
 
-        else:
-            raise ValueError("Unsupported database type. Use 'postgres' or 'sqlite'.")
+            else:
+                raise ValueError("Unsupported database type. Use 'postgres' or 'sqlite'.")
 
     def get_connection_string(self):
         if self.db_type == "postgres":
